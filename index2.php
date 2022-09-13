c6f812f0e5d6ee4996ff153ccdfb5740bc98a227
Jose Olivera <jolivera@educ.ar>
2022-09-13 12:10:03 -0300

    Agregando Cambios

--- index2.php
+++ index2.php
@@ -3,3 +3,8 @@ Mi primer
 [9:42, 14/6/2022] #yotevicampeón: Curso 1 División G
 [9:42, 14/6/2022] #yotevicampeón: DNI 26977512
 [9:48, 14/6/2022] #yotevicampeón: El día Miércoles 14/09/22  tomo licencia 23.1a. Gracias
+[9:42, 14/6/2022] #yotevicampeón: Curso 1 División B
+[9:42, 14/6/2022] #yotevicampeón: DNI 26977512
+[9:48, 14/6/2022] #yotevicampeón: El día Miércoles 15 de Junio tomo licencia 23.1a. Gracias[9:42, 14/6/2022] #yotevicampeón: Curso 1 División B
+[9:42, 14/6/2022] #yotevicampeón: DNI 26977512
+[9:48, 14/6/2022] #yotevicampeón: El día Miércoles 15 de Junio tomo licencia 23.1a. Gracias
