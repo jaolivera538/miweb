c57e133ca40fd0b87e37b9c695eb0e1ac91f5a83
Jose Olivera <jolivera@educ.ar>
2022-09-13 12:13:29 -0300

    Cambios de index02

--- index2.php
+++ index2.php
@@ -8,3 +8,5 @@ Mi primer
 [9:48, 14/6/2022] #yotevicampeón: El día Miércoles 15 de Junio tomo licencia 23.1a. Gracias[9:42, 14/6/2022] #yotevicampeón: Curso 1 División B
 [9:42, 14/6/2022] #yotevicampeón: DNI 26977512
 [9:48, 14/6/2022] #yotevicampeón: El día Miércoles 15 de Junio tomo licencia 23.1a. Gracias
+cencia 23.1a. Graccencia 23.1a. Graccencia 23.1a. Grac
+cencia 23.1a. Graccencia 23.1a. Grac
