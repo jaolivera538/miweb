f0dfb693257f2f6d5fa6fc9b5e1d10153ce6a969
Jose Olivera <jolivera@educ.ar>
2022-09-13 11:44:08 -0300

    Mostrando status corto

--- index.php
+++ index.php
@@ -2,3 +2,6 @@ Mi primer aplicacion de desarrollo
 Mi primer aplicacion de desarrollo
 Mi primer aplicacion de desarrollo
 Mi primer aplicacion de desarrollo
+[9:42, 14/6/2022] #yotevicampeón: Curso 1 División B
+[9:42, 14/6/2022] #yotevicampeón: DNI 26977512
+[9:48, 14/6/2022] #yotevicampeón: El día Miércoles 15 de Junio tomo licencia 23.1a. Gracias
--- index2.php
+++ index2.php
@@ -1,2 +1,5 @@
 Mi primer aplicacion de desarrollo
 Mi primer 
+[9:42, 14/6/2022] #yotevicampeón: Curso 1 División G
+[9:42, 14/6/2022] #yotevicampeón: DNI 26977512
+[9:48, 14/6/2022] #yotevicampeón: El día Miércoles 14/09/22  tomo licencia 23.1a. Gracias
